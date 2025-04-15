72211daaf1351ce8765168acbffe782ba7780df6
mehakkd <70606001+mehakkd@users.noreply.github.com>
2025-04-14 20:57:48 -0400

    Create Song.java

---
+++ Song.java
@@ -0,0 +1,29 @@
+import java.util.LinkedList;
+
+public class Song {
+    LinkedList<String> artistNames;
+    String trackName;
+
+    public void main(LinkedList<String> artistNames, String trackName){
+        this.artistNames = artistNames;
+        this.trackName = trackName;
+    }
+
+    public String getTrackName(){
+        return trackName;
+    }
+
+    public LinkedList<String> getArtistNames(){
+        return artistNames;
+    }
+
+    public boolean checkSharesArtist(Song other){
+        LinkedList<String> otherSongArtistNames = other.getArtistNames();
+        for (String artist: artistNames){
+            if (otherSongArtistNames.contains(artist)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
